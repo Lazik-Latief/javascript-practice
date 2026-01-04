2d1692617c0b15979759dc56d41830b54e420395
lazik <laziklatief@gmail.com>
2026-01-04 15:39:05 +0530

    practiced destructuring

--- Task02-js/script.js
+++ Task02-js/script.js
@@ -1,91 +1,154 @@
+// Creating an object oj1 with different properties
 let oj1 = {
-  name: "Lazik",
-  age: 22,
+  name: "Lazik",          // string value
+  age: 22,               // number value
+
+  // nested object (object inside object)
   address: {
     city: "srinagar",
     village: "watlab",
   },
-  Number: 9797685148,
-  working: true,
+
+  Number: 9797685148,     // phone number
+  working: true,         // boolean value
 };
 
-console.log(oj1);
-oj1.address.village = "nowpore"
+// Print complete object oj1
 console.log(oj1);
 
-oj2 ={
+// Update nested object property
+oj1.address.village = "nowpore";
+
+// Print updated object
+console.log(oj1);
+
+// Create another object oj2
+let oj2 = {
   name: "lone",
-  age : 24,
+  age: 24,
   job: false,
+};
 
-}
+// Print oj2
 console.log(oj2);
 
-oj3 = {oj1, oj2}
+// Create object that contains oj1 and oj2 as properties
+let oj3 = { oj1, oj2 };
 
+// Print nested objects
 console.log(oj3);
-oj4 ={...oj1, ...oj2}
+
+// Merge oj1 and oj2 using spread operator
+// NOTE: same keys will be overwritten by oj2
+let oj4 = { ...oj1, ...oj2 };
+
+// Print merged object
 console.log(oj4);
 
-
+// Access object value using bracket notation
 console.log(oj1["age"]);
-const mySym = Symbol("key1")
 
+// Create a symbol (unique data type)
+const mySym = Symbol("key1");
+
+// Create object with a normal key named "mySym"
 let jsUser = {
-  mySym : "myKey1"
+  mySym: "myKey1",
+};
 
-}
+// Print object
 console.log(jsUser);
+
+// Print type of symbol
 console.log(typeof mySym);
 
-let  oj7 = Object.assign(oj1, oj2, jsUser)
+// Merge objects using Object.assign()
+// Target object is oj1 (it will be modified)
+let oj7 = Object.assign(oj1, oj2, jsUser);
+
+// Print merged result
 console.log(oj7);
 
-function hey(){
+// Simple function
+function hey() {
   console.log("hello ");
-  
-} 
-hey()
-console.log(oj1, hey());
-let kkk = "azhar lone"
-console.log(`hello js user , ${hey}`);
-console.log(`hiii , ${kkk}`);
-
-function g(){
-  "hii jnnii "
 }
 
+// Call function
+hey();
+
+// Log object and function call result
+console.log(oj1, hey());
+
+// String variable
+let kkk = "azhar lone";
+
+// Template literal (function reference printed, not called)
+console.log(`hello js user , ${hey}`);
+
+// Template literal with variable
+console.log(`hiii , ${kkk}`);
+
+// Function with no return value
+function g() {
+  "hii jnnii ";
+}
+
+// Template literal (function NOT executed here)
 console.log(`g(),${kkk}`);
 
+// Create a new object newObj
 let newObj = {
   name: "azhar lone",
   age: 23,
   study: true,
   job: false,
-  address:{
-    villege:  "Watlab",
-    City: "Srinagar"
+
+  // nested object
+  address: {
+    villege: "Watlab",
+    City: "Srinagar",
   },
+
   number: 7889943194,
   learningReact: true,
+};
 
-}
+// Print object
 console.log(newObj);
 
-newObj.number =9797685148;
+// Update object property
+newObj.number = 9797685148;
 console.log(newObj);
 
+// Freeze object (no changes allowed now)
 Object.freeze(newObj);
+
+// This change will NOT work because object is frozen
 newObj.number = 7889943194;
 
+// Print object after freeze
 console.log(newObj);
 
+// Create course object
+const course = {
+  name: "Lukman Latief",
+  course: "Web Development",
+  teacher: "Lazik",
+  price: 4999,
+};
 
+// Access property normally
+course.teacher;
 
+// Object destructuring
+const { teacher } = course;
+console.log(teacher);
 
+// Destructure name
+const { name } = course;
+console.log(name);
 
-
-
-
-
-
+// Destructure and rename variable
+const { teacher: sir } = course;
+console.log(sir);
