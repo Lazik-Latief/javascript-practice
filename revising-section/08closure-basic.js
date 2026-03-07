6a0f554538bc2397d515aae3afc3e73ba86db7e6
Lazik-Latief <laziklatief99@gmail.com>
2026-03-07 18:23:27 +0530

    revising-

--- revising-section/08closure-basic.js
+++ revising-section/08closure-basic.js
@@ -13,3 +13,44 @@ counter(); // 1
 counter(); // 2
 counter(); // 3
 
+// 
+
+function setupButton(buttonId) {
+  let clickCount = 0;
+
+  document.getElementById(buttonId).addEventListener("click", () => {
+    clickCount++;
+    console.log("Button clicked", clickCount, "times");
+  });
+}
+
+setupButton("myButton");
+// 
+
+function bankAccount(initialBalance) {
+  let balance = initialBalance;
+
+  return {
+    deposit(amount) {
+      balance += amount;
+      console.log("Balance:", balance);
+    },
+
+    withdraw(amount) {
+      balance -= amount;
+      console.log("Balance:", balance);
+    },
+
+    getBalance() {
+      return balance;
+    },
+  };
+}
+
+const account = bankAccount(100);
+
+account.deposit(50);   // 150
+account.withdraw(20);  // 130
+
+console.log(account.getBalance());
+
